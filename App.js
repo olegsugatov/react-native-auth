6672d28bc16b7cdda02b0c90f88a3c41c2e0786e
olegsugatov <oleg.sugatov@gmail.com>
2017-05-21 23:30:27 +0300

    navigatorIOS

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigatorIOS,
+         StyleSheet, 
+         Text, 
+         View } from 'react-native';
+
 import Splash from './Splash';
 import Login from './src/components/Login/Login';
+import Main from './src/components/Main';
 
 export default class App extends React.Component {
   render() {
     return (
-        <Login />
+      <NavigatorIOS 
+        initialRoute = {{
+          title: 'Github Networking'
+          component: Main
+        }}/>
+        // <Login />
+        // <Main />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+var React = require('react-native');
+
+import { NavigatorIOS,
+         StyleSheet, 
+         Text, 
+         View } from 'react-native';
+
+class Main extends React.Component {
+	render() {
+		return () {
+			<View>
+				<Text> Testing the routing </Text>
+			</View>
+		}
+	}
+}
+
+module.exports = Main;
